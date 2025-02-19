03807196ca3266b75578bf067e25a9b5ea015bd6
Tehilah Zamanzadeh <tehilah.zaman@gmail.com>
2025-02-19 15:32:53 -0800

    established booking controller outline

---
+++ controllers/bookings.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const router = express.Router();
+const Booking = require("../models/booking.js");
+const verifyToken = require("../middleware/verify-token");
+
+// CRUD functions :
+// index and show
+// users cannot create, edit, or delete yet
+
+
+module.exports = router;
