60b65578bf63b0c9f1d759c6e6059e58da520106
Tehilah Zamanzadeh <tehilah.zaman@gmail.com>
2025-02-19 15:43:36 -0800

    booking index

--- controllers/bookings.js
+++ controllers/bookings.js
@@ -3,9 +3,21 @@ const router = express.Router();
 const Booking = require("../models/booking.js");
 const verifyToken = require("../middleware/verify-token");
 
-// CRUD functions :
+// CRUD functions needed:
 // index and show
 // users cannot create, edit, or delete yet
+
+//route mounted at /users/bookings
+
+router.get("/", verifyToken, async (req, res) => {
+  try {
+    const bookings = await Booking.find();
+    res.status(200).json(bookings);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ err: err.message });
+  }
+});
 
 
 module.exports = router;
